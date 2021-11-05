bc99a75f1b6afd2e02d8246c8bee77c75dab5f83
Kevin <sp8va100@gmail.com>
2021-11-05 17:13:40 +0800

    fix path

--- scripts/test_inference.sh
+++ scripts/test_inference.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
-python ./src/autozoom.py --in ./src/images/doublestrike.jpg --out /src./movies/doublestrike.mp4
+python ../src/autozoom.py --in ../src/images/doublestrike.jpg --out ../src/movies/doublestrike.mp4
 
-if [ -f "./src/movies/doublestrike.mp4" ]; then
+if [ -f "../src/movies/doublestrike.mp4" ]; then
     echo "Inference Success."
-    rm ./src/movies/doublestrike.mp4
+    rm ../src/movies/doublestrike.mp4
 else
     echo "Inference Failure"
 fi
