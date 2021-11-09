8e4903bda84748055238d75ddddc6c4487113eec
Kevin <sp8va100@gmail.com>
2021-11-09 10:05:46 +0800

    add web service

---
+++ src/main.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import torch
+import torchvision
+import cupy
+import cv2
+import getopt
+import glob
+import h5py
+import io
+import math
+import moviepy
+import moviepy.editor
+import numpy
+import os
+import random
+import re
+import scipy
+import scipy.io
+import sys
+import time
+import tempfile
+import zipfile
+import requests
+
+from fastapi import FastAPI, FileResponse
+
+##########################################################
+
+assert(int(str('').join(torch.__version__.split('.')[0:2])) >= 12) # requires at least pytorch version 1.2.0
+
+torch.set_grad_enabled(False) # make sure to not compute gradients for computational performance
+
+torch.backends.cudnn.enabled = True # make sure to use cudnn for computational performance
+
+##########################################################
+
+objCommon = {}
+
+exec(open('./common.py', 'r').read())
+
+exec(open('./models/disparity-estimation.py', 'r').read())
+exec(open('./models/disparity-adjustment.py', 'r').read())
+exec(open('./models/disparity-refinement.py', 'r').read())
+exec(open('./models/pointcloud-inpainting.py', 'r').read())
+
+##########################################################
+
+app = FastAPI()
+
+app.post("/input")
+def autozoom(url: str):
+	path = os.join(os.curdir, "movies")
+	fileList = [i for i in os.listdir(path)]
+	if len(fileList) > 0:
+		for f in fileList:
+			os.remove(os.join(path, f))
+	
+    img_data = requests.get(url, stream=True).raw
+    npyImage = numpy.asarray(bytearray(img_data.read(), dtype='uint8'))
+    npyImage = numpy.ascontiguousarray(cv2.imdecode(npyImage, cv2.IMREAD_COLOR))
+    intWidth = npyImage.shape[1]
+	intHeight = npyImage.shape[0]
+
+	fltRatio = float(intWidth) / float(intHeight)
+
+	intWidth = min(int(1024 * fltRatio), 1024)
+	intHeight = min(int(1024 / fltRatio), 1024)
+
+	npyImage = cv2.resize(src=npyImage, dsize=(intWidth, intHeight), fx=0.0, fy=0.0, interpolation=cv2.INTER_AREA)
+
+	process_load(npyImage, {})
+
+	objFrom = {
+		'fltCenterU': intWidth / 2.0,
+		'fltCenterV': intHeight / 2.0,
+		'intCropWidth': int(math.floor(0.97 * intWidth)),
+		'intCropHeight': int(math.floor(0.97 * intHeight))
+	}
+
+	objTo = process_autozoom({
+		'fltShift': 100.0,
+		'fltZoom': 1.25,
+		'objFrom': objFrom
+	})
+
+	npyResult = process_kenburns({
+		'fltSteps': numpy.linspace(0.0, 1.0, 75).tolist(),
+		'objFrom': objFrom,
+		'objTo': objTo,
+		'boolInpaint': True
+	})
+
+	outputPath = os.join(path, "kenburns.mp4")
+	moviepy.editor.ImageSequenceClip(sequence=[ npyFrame[:, :, ::-1] for npyFrame in npyResult + list(reversed(npyResult))[1:] ], fps=25).write_videofile(outputPath)
+
+	return FileResponse(outputPath, media_type='text/mp4', filename='kenburns.mp4')
